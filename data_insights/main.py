4a8c21706a20ee3cf7b982215b498ebd0ee03740
Prabesh Rai <raiprabesh789@gmail.com>
2025-02-27 12:00:55 -0500

    Update: extract data file path updated.

--- data_insights/main.py
+++ data_insights/main.py
@@ -3,7 +3,7 @@ import zipfile
 from src.dataset_factory import DatasetFactory
 
 DATA_FOLDER = "data_insights/data"
-EXTRACTED_FOLDER = "extracted_data"  # Define extraction folder dynamically
+EXTRACTED_FOLDER = "data_insights/extracted_data"  # Define extraction folder dynamically
 
 def extract_zip(zip_path):
     """Extract ZIP file into 'extracted_data/' and return extracted file paths."""
